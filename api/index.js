d469bd5ea6df92d8d06632ad14a674825f9e9661
Fcltd1 <joacofcltd@gmail.com>
2022-10-27 17:45:43 -0300

    l

--- api/index.js
+++ api/index.js
@@ -4,5 +4,5 @@ const sequelize = require('./src/db');
 
 // (se declara una variable de entorno con el puerto (3001)) o (heroku provee un puerto)
 sequelize.sync({ force: false }).then(
-    () => app.listen(process.env.PORT, () => console.log('listening') )
+    () => app.listen(process.env.PORT)
 );
