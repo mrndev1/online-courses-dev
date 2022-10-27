28961a2e72644d0cfe59a8433b200b6ab10b9d9e
Fcltd1 <joacofcltd@gmail.com>
2022-10-27 17:37:24 -0300

    l

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+const { Router, json } = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./routes/index');
@@ -8,7 +8,7 @@ const app = Router();
 app.name = 'API';
 app.use(cors('*'));
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(json());
 app.use('/', routes);
 
 
